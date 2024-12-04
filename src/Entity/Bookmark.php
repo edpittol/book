729423358d6fd6b997d92efdd6d35823d3c960b8
Eduardo Pittol <edpittol@gmail.com>
2024-12-04 08:01:09 -0300

    Add bookmark entity

---
+++ src/Entity/Bookmark.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookmarkRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookmarkRepository::class)]
+#[ORM\UniqueConstraint(name: "google_books_id", columns: ["google_books_id"])]
+class Bookmark
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 12)]
+    private ?string $google_books_id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getGoogleBooksId(): ?string
+    {
+        return $this->google_books_id;
+    }
+
+    public function setGoogleBooksId(string $google_books_id): static
+    {
+        $this->google_books_id = $google_books_id;
+
+        return $this;
+    }
+}
