c49fa89b3a7ebd7635cfe24d6c835ec053a5037b
Eduardo Pittol <edpittol@gmail.com>
2024-12-30 17:46:43 -0300

    Add Rector

---
+++ rector.php
@@ -0,0 +1,52 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
+use Rector\Symfony\Set\SymfonySetList;
+
+return RectorConfig::configure()
+    ->withSymfonyContainerXml(__DIR__.'/var/cache/dev/App_KernelDevDebugContainer.xml')
+    ->withPaths([
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ])
+    ->withSkip([
+        __DIR__.'/tests/Support/_generated/*',
+        RenameParamToMatchTypeRector::class,
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        codingStyle: true,
+        typeDeclarations: true,
+        privatization: true,
+        naming: true,
+        instanceOf: true,
+        earlyReturn: true,
+        strictBooleans: true,
+        carbon: true,
+        rectorPreset: true,
+        phpunitCodeQuality: true,
+        doctrineCodeQuality: true,
+        symfonyCodeQuality: true,
+        symfonyConfigs: true,
+    )
+    ->withPhpSets(
+        php84: true,
+    )
+    ->withAttributesSets(
+        symfony: true,
+        doctrine: true,
+        phpunit: true,
+    )
+    ->withComposerBased(
+        doctrine: true,
+        phpunit: true,
+        twig: true
+    )
+    ->withSets([
+        SymfonySetList::SYMFONY_72,
+        SymfonySetList::SYMFONY_CODE_QUALITY,
+        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
+    ])
+;
