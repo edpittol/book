5c54868b010d0ca27786acbce7c78bdaa9fd672d
Eduardo Pittol <edpittol@gmail.com>
2025-01-03 23:10:02 -0300

    Add php-cs-fixer to qa scripts group

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -10,4 +10,5 @@ return (new PhpCsFixer\Config())
         '@Symfony' => true,
     ])
     ->setFinder($finder)
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
 ;
