8e2852ca5061d0f89fa659f3bcced120873a361c
Eduardo Pittol <edpittol@gmail.com>
2025-01-06 12:22:24 -0300

    Build Codeception during the installation

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,7 +2,10 @@
 
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
-    ->exclude('var')
+    ->exclude([
+        'var',
+        'tests/Support/_generated',
+    ])
 ;
 
 return (new PhpCsFixer\Config())
